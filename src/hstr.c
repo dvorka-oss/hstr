16ca775f360cc47a3b7fdd529e7548341a820bcd
Martin Dvorak <martin.dvorak@mindforger.com>
2026-01-26 08:16:49 +0100

    bug(views): fixing buffer overflow vulnerability in HSTR that caused crashes when pressing Ctrl+/ - on wide terminals (>2048 characters), fixed CMDLINE_LNG caused a buffer overflow, corrupting memory and crashing the application when wgetch() tried to read input.

--- src/hstr.c
+++ src/hstr.c
@@ -768,7 +768,7 @@ void print_help_label(void)
     int cursorY=getcury(stdscr);
 
     char screenLine[CMDLINE_LNG];
-    snprintf(screenLine, getmaxx(stdscr), "%s", LABEL_HELP);
+    snprintf(screenLine, sizeof(screenLine), "%s", LABEL_HELP);
     mvprintw(hstr->promptYHelp, 0, "%s", screenLine); clrtoeol();
     refresh();
 
@@ -779,9 +779,9 @@ void print_confirm_delete(const char* cmd)
 {
     char screenLine[CMDLINE_LNG];
     if(hstr->view==HSTR_VIEW_FAVORITES) {
-        snprintf(screenLine, getmaxx(stdscr), "Do you want to delete favorites item '%s'? y/n", cmd);
+        snprintf(screenLine, sizeof(screenLine), "Do you want to delete favorites item '%s'? y/n", cmd);
     } else {
-        snprintf(screenLine, getmaxx(stdscr), "Do you want to delete all occurrences of '%s'? y/n", cmd);
+        snprintf(screenLine, sizeof(screenLine), "Do you want to delete all occurrences of '%s'? y/n", cmd);
     }
     // TODO make this function
     if(hstr->theme & HSTR_THEME_COLOR) {
@@ -801,9 +801,9 @@ void print_cmd_deleted_label(const char* cmd, int occurences)
 {
     char screenLine[CMDLINE_LNG];
     if(hstr->view==HSTR_VIEW_FAVORITES) {
-        snprintf(screenLine, getmaxx(stdscr), "Favorites item '%s' deleted", cmd);
+        snprintf(screenLine, sizeof(screenLine), "Favorites item '%s' deleted", cmd);
     } else {
-        snprintf(screenLine, getmaxx(stdscr), "History item '%s' deleted (%d occurrence%s)", cmd, occurences, (occurences==1?"":"s"));
+        snprintf(screenLine, sizeof(screenLine), "History item '%s' deleted (%d occurrence%s)", cmd, occurences, (occurences==1?"":"s"));
     }
     // TODO make this function
     if(hstr->theme & HSTR_THEME_COLOR) {
@@ -822,7 +822,7 @@ void print_cmd_deleted_label(const char* cmd, int occurences)
 void print_regexp_error(const char* errorMessage)
 {
     char screenLine[CMDLINE_LNG];
-    snprintf(screenLine, getmaxx(stdscr), "%s", errorMessage);
+    snprintf(screenLine, sizeof(screenLine), "%s", errorMessage);
     if(hstr->theme & HSTR_THEME_COLOR) {
         color_attr_on(COLOR_PAIR(HSTR_COLOR_DELETE));
         color_attr_on(A_BOLD);
@@ -839,7 +839,7 @@ void print_regexp_error(const char* errorMessage)
 void print_cmd_added_favorite_label(const char* cmd)
 {
     char screenLine[CMDLINE_LNG];
-    snprintf(screenLine, getmaxx(stdscr), "Command '%s' added to favorites (C-/ to show favorites)", cmd);
+    snprintf(screenLine, sizeof(screenLine), "Command '%s' added to favorites (C-/ to show favorites)", cmd);
     if(hstr->theme & HSTR_THEME_COLOR) {
         color_attr_on(COLOR_PAIR(HSTR_COLOR_INFO));
         color_attr_on(A_BOLD);
@@ -862,9 +862,9 @@ void print_history_label(void)
 
     char screenLine[CMDLINE_LNG];
 #ifdef __APPLE__
-    snprintf(screenLine, width, "- HISTORY - view:%s (C-w) - match:%s (C-e) - case:%s (C-t) - %d/%d/%d ",
+    snprintf(screenLine, sizeof(screenLine), "- HISTORY - view:%s (C-w) - match:%s (C-e) - case:%s (C-t) - %d/%d/%d ",
 #else
-    snprintf(screenLine, width, "- HISTORY - view:%s (C-/) - match:%s (C-e) - case:%s (C-t) - %d/%d/%d ",
+    snprintf(screenLine, sizeof(screenLine), "- HISTORY - view:%s (C-/) - match:%s (C-e) - case:%s (C-t) - %d/%d/%d ",
 #endif
             HSTR_VIEW_LABELS[hstr->view],
             HSTR_MATCH_LABELS[hstr->matching],
@@ -872,9 +872,13 @@ void print_history_label(void)
             hstr->history->count,
             hstr->history->rawCount,
             hstr->favorites->count);
-    width -= strlen(screenLine);
+    unsigned lineLen = strlen(screenLine);
+    unsigned dashCount = (width > lineLen) ? (width - lineLen) : 0;
+    if (dashCount > sizeof(screenLine) - lineLen - 1) {
+        dashCount = sizeof(screenLine) - lineLen - 1;
+    }
     unsigned i;
-    for(i=0; i<width; i++) {
+    for(i=0; i<dashCount; i++) {
         strcat(screenLine, "-");
     }
     if(hstr->theme & HSTR_THEME_COLOR) {
@@ -1150,9 +1154,10 @@ void hstr_print_highlighted_selection_row(char* text, int y, int width)
     char buffer[CMDLINE_LNG];
     hstr_strelide(buffer, text, width>2?width-2:0);
     char screenLine[CMDLINE_LNG];
-    snprintf(screenLine, getmaxx(stdscr)+1, "%s%-*.*s ",
+    unsigned maxWidth = getmaxx(stdscr);
+    snprintf(screenLine, sizeof(screenLine), "%s%-*.*s ",
             (terminal_has_colors()?" ":">"),
-            getmaxx(stdscr)-2, getmaxx(stdscr)-2, buffer);
+            maxWidth-2, maxWidth-2, buffer);
     mvprintw(y, 0, "%s", screenLine);
     if(hstr->theme & HSTR_THEME_COLOR) {
         color_attr_on(COLOR_PAIR(HSTR_COLOR_NORMAL));
